225011bc707861b4c1ed2b3651558424a2325171
Meny <maya001@cougars.csusm.edu>
2019-09-23 18:11:25 -0700

    Game With Bullets
    Current Version
    Test

---
+++ FinalGame/Assets/Scripts/Bullet_Controller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float BulletSpeed;
+
+    public float damage;
+
+    private void Start()
+    {
+    	transform.GetComponent<Rigidbody>().velocity = transform.forward * BulletSpeed;
+
+
+
+    }
+
+    private void Update()
+    {
+    	Destroy(gameObject,2);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+    	if (collision.gameObject.tag == "Asteroid")
+    	{
+    		collision.transform.GetComponent<Asteroid_Controller>().stats.currentHealth -= damage;
+    		Destroy(gameObject);
+
+    	}
+    	
+
+    }
+}
