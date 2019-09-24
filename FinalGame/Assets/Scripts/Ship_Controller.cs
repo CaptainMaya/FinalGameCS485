1e853b81cbfd5b172013335158a3d86420a5c92b
Meny <maya001@cougars.csusm.edu>
2019-09-23 18:11:25 -0700

    Game With Bullets
    Current Version

---
+++ FinalGame/Assets/Scripts/Ship_Controller.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship_Controller : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public GameObject bullet;
+    public Transform[] firePoints = new Transform[2];
+    public float fireRate;
+    private float nextFire;
+
+
+    public float moveSpeed;
+    public float tiltAngle;
+
+    private void Start()
+    {
+        rb = transform.GetComponent<Rigidbody>(); //gets the outter shell of the ship
+        nextFire = 1 / fireRate; //how fast the bullets will be fired
+        //nextFire += 1 / fireRate;
+    }
+
+    private void FixedUpdate()
+    {
+        float moveLR = Input.GetAxis("Horizontal"); //move left to right
+        float moveFB = Input.GetAxis("Vertical"); //move front and forward
+
+        Vector3 movement = new Vector3(moveLR, 0, moveFB);
+        rb.velocity = movement * moveSpeed;
+
+        rb.rotation = Quaternion.Euler(Vector3.forward * moveLR * -tiltAngle);
+
+        bool fireButton = Input.GetButton("Fire1");
+
+        Collider[] shipColliders = transform.GetComponentsInChildren<Collider>();
+
+        if(fireButton)
+        {
+            nextFire -= Time.fixedDeltaTime;
+            if(nextFire <= 0)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    GameObject bulletClone = Instantiate(bullet, firePoints[i].position, Quaternion.Euler(0, 0, 0));
+
+                    for(int x = 0; x < shipColliders.Length; x++)
+                    {
+                    	Physics.IgnoreCollision(bulletClone.transform.GetComponent<Collider>(), shipColliders[x]);
+
+                    }
+
+
+                }
+               nextFire += 1 / fireRate;
+
+            }
+
+        }
+    }
+}
